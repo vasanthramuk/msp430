507de29cdea73b8d4dfe9a29aea0e8ec988798dc
vasanthramuk <vasanthvv1717@gmail.com>
2021-01-14 21:38:20 +0530

    added few more functions to use UART and SPI simultaneously
    The program receives currentTime from RTC, formats it and sends it to PC
    via UART

--- interfacingDS1306viaSPI/main.c
+++ interfacingDS1306viaSPI/main.c
@@ -1,13 +1,29 @@
 #include<msp430.h>
 #include<stdint.h>
 
-//
+//=====================GPIO===============//
 #define LED BIT0
 #define CE BIT3
 #define SPI_SOMI BIT6
 #define SPI_SIMO BIT7
 #define SPI_CLK BIT5
+#define UART_TX BIT2
+#define UART_RX BIT1
+//========================================//
 
+#define UPNIBBLE(x) (x >> 4) + '0'
+#define LOWNIBBLE(x) (x & 0x0f) + '0'
+
+//SECOND-0, MINUTE-1, HOUR-2, DAY-3, DATE-4, MONTH-5, YEAR-6
+#define FORMAT_DATE currentTime[4]
+#define FORMAT_MONTH currentTime[5]
+#define FORMAT_YEAR currentTime[6]
+#define FORMAT_HOUR currentTime[2]
+#define FORMAT_MINUTE currentTime[1]
+#define FORMAT_SECOND currentTime[0]
+
+
+//=========================RTC===================//
 #define RTCCTL_WRITE 0x8F
 #define RTCCTL_READ 0x0F
 
@@ -19,6 +35,10 @@
 #define RTCSTAT 0x10
 #define IRQF1 BIT1
 #define IRQF0 BIT0
+//================================================//
+
+
+#define FORMATTED_TIME_LENGTH 18
 
 //=============================CURRENT TIME AND DAY================================//
 #define SECOND 0x00
@@ -41,14 +61,18 @@ void waitForTX();
 void waitForRX();
 void setRTCLoop();
 void readRTCLoop();
+void printTimeOnPC();
+void prepareTimeForPC();
 
 //=============================Global variables=============================//
 uint8_t timeProfile[7] = {SECOND, MINUTE, HOUR, DAY, DATE, MONTH, YEAR};         //The first element of the array contains the array from which the
-uint8_t currentTime[7];                                                      //This array would have the most recent info from RTC
-uint8_t TXCount, RXCount;                                                    //To control the number of TX and RX
 
+volatile uint8_t currentTime[7];                                                      //This array would have the most recent info from RTC
 
-int main(){
+volatile uint8_t timeForPC[FORMATTED_TIME_LENGTH];                              //This array contains formatted time to be sent via UART
+volatile uint8_t TXCount, RXCount;                                                    //To control the number of TX and RX
+
+void main(){
 
 
     WDTCTL = WDTPW + WDTHOLD;
@@ -56,19 +80,21 @@ int main(){
 
     //Configuring DCO to operate at 1 MHz
     DCOCTL = 0;
-    BCSCTL1 = CALBC1_1MHZ;
-    DCOCTL = CALDCO_1MHZ;
+    BCSCTL1 = 135;
+    DCOCTL = 47;
 
 
     configPort();
     configSPI();
+    configUART();
 
     while(1){
     readRTC();
+    prepareTimeForPC();
 
+    printTimeOnPC();
     }
 
-    return 0;
 }
 
 //=============================Interrupt Service Routines=============================//
@@ -76,26 +102,38 @@ int main(){
 #pragma vector = USCIAB0RX_VECTOR
 __interrupt void RX_ISR(void){
     if(IFG2 & UCB0RXIFG){
-        currentTime[RXCount] = UCB0RXBUF;         //This updates currentTime with RTC value
+        currentTime[RXCount++] = UCB0RXBUF;         //This updates currentTime with RTC value
                                                     //It also acknowledges the interrupt by clearing UCB0RXIFG
-        RXCount++;
         if(RXCount > 7){
             IE2 &= ~UCB0RXIE;                           //After reception of all 7 bytes, disable further RX interrupts
+            IFG2 &= ~UCB0RXIFG;
             __bic_SR_register_on_exit(LPM4_bits + GIE); //Also exit LPM4
         }
     }
+
 
 }
 
 #pragma vector = USCIAB0TX_VECTOR
 __interrupt void TX_ISR(void){
-    if(IFG2 & UCB0TXIFG){               //TO make sure the interrupt is caused by USCIB0
+    if((IFG2 & UCB0TXIFG) && (IE2 & UCB0TXIE)){               //TO make sure the interrupt is caused by USCIB0
 
        UCB0TXBUF = 0x55;       //This sends 0x55 which has no significance as we are only interested in RXed data
                                 //This acknowledges interrupt by clearing UCB0TXIFG
        TXCount++;
         if(TXCount > 7){                    //Since we are Transmitting 7 bytes
             IE2 &= ~UCB0TXIE;
+        }
+    }
+
+    if((IFG2 & UCA0TXIFG) && (IE2 & UCA0TXIE)){
+
+        UCA0TXBUF = timeForPC[TXCount];            //This would acknowledge by clearing UCA0TXIFG
+
+        TXCount++;
+        if(TXCount >= FORMATTED_TIME_LENGTH){
+            IE2 &= ~UCA0TXIE;
+            __bic_SR_register_on_exit(LPM4_bits + GIE);
         }
     }
 
@@ -110,14 +148,14 @@ void configPort(){
     P2DIR |= CE;
 
     //Configuring USCIB0
-    P1SEL = SPI_SOMI + SPI_SIMO + SPI_CLK;
-    P1SEL2 = SPI_SOMI + SPI_SIMO + SPI_CLK;
+    P1SEL = SPI_SOMI + SPI_SIMO + SPI_CLK + UART_TX + UART_RX;
+    P1SEL2 = SPI_SOMI + SPI_SIMO + SPI_CLK + UART_TX + UART_RX;
 
-    P1REN |= ~(SPI_SOMI + SPI_SIMO + SPI_CLK);      //Unused pulled-down
-    P1OUT &= SPI_SOMI + SPI_SIMO + SPI_CLK;
+    P1REN |= ~(SPI_SOMI + SPI_SIMO + SPI_CLK + UART_TX + UART_RX);      //PORT1 Unused pulled-down
+    P1OUT &= SPI_SOMI + SPI_SIMO + SPI_CLK + UART_TX + UART_RX;
 
 
-    P2REN |= ~CE ;       //Unused pulled-down
+    P2REN |= ~CE ;                                                      //PORT2 Unused pulled-down
     P2OUT &= CE;
 
 }
@@ -170,7 +208,7 @@ void enableWriteProtectionRTC(uint8_t option){
 }
 
 void configUART(void){
-    //No parity, 8-bit, LSB first,  1 SPB
+    //9600 BAUD, No parity, 8-bit, LSB first,  1 SPB
 
     UCA0CTL1 = UCSWRST + UCSSEL_2;      //Choose BRCLK from SMCL operating @ 1 MHz
 
@@ -178,9 +216,7 @@ void configUART(void){
     UCA0BR1 = 0;
     UCA0MCTL = UCBRS_1;                 //9600-8-N configuration for UART
 
-
-    IE2 |= UCA0TXIE;
-
+    UCA0CTL1 &= ~UCSWRST;
 }
 
 void readRTC(){
@@ -200,6 +236,38 @@ void readRTC(){
 
 
     P2OUT &= ~CE;
+}
+
+void prepareTimeForPC(){
+
+    //Format : dd/mm/yy
+        timeForPC[0] = UPNIBBLE(FORMAT_DATE);
+        timeForPC[1] = LOWNIBBLE(FORMAT_DATE);
+        timeForPC[2] = '\\';
+        timeForPC[3] = UPNIBBLE(FORMAT_MONTH);
+        timeForPC[4] = LOWNIBBLE(FORMAT_MONTH);
+        timeForPC[5] = '\\';
+        timeForPC[6] = UPNIBBLE(FORMAT_YEAR);
+        timeForPC[7] = LOWNIBBLE(FORMAT_YEAR);
+        timeForPC[8] = ' ';
+
+        //Format : hh:mm:ss
+        timeForPC[9] = UPNIBBLE(FORMAT_HOUR);
+        timeForPC[10] = LOWNIBBLE(FORMAT_HOUR);
+        timeForPC[11] = ':';
+        timeForPC[12] = UPNIBBLE(FORMAT_MINUTE);
+        timeForPC[13] = LOWNIBBLE(FORMAT_MINUTE);
+        timeForPC[14] = ':';
+        timeForPC[15] = UPNIBBLE(FORMAT_SECOND);
+        timeForPC[16] = LOWNIBBLE(FORMAT_SECOND);
+        timeForPC[17] = ' ';
+
+}
+void printTimeOnPC(){
+    IE2 |= UCA0TXIE;
+    TXCount = 0;                            //To keep a tab on number of Bytes sent
+    __bis_SR_register(LPM4_bits + GIE);     //Go to sleep and wait till TXBUF is empty. When empty, service the ISR
+
 }
 
 void waitForTX(){
@@ -227,6 +295,7 @@ void setRTCLoop(){                              //Sets the current time in RTC b
 
     enableWriteProtectionRTC(1);
 }
+
 
 void readRTCLoop(){                             //Read and place the current time in currentTime[] by a blocking routine
 
